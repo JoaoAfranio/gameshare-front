0e25be77e389062ccd2a8fbfe88ed0765981525e
aleunamp <manuella.m.p182@gmail.com>
2022-11-25 12:10:05 -0300

    feat: sign-in, sign-up and sucess screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import GlobalStyle from "./assets/css/GlobalStyle";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Games from "./pages/Games";
 import InfoGame from "./pages/InfoGame";
 import CategoryGames from "./pages/CategoryGames";
 import Profile from "./pages/Profile";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import SucessPage from "./pages/SucessPage";
 import CartProvider from "./Contexts/CartContext";
 
 function App() {
@@ -13,10 +15,13 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
           <Route path="/games" element={<Games />} />
           <Route path="/games/:id" element={<InfoGame />} />
           <Route path="/category" element={<CategoryGames />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/sucess" element={<SucessPage/>} />
         </Routes>
       </BrowserRouter>
     </CartProvider>
---
+++ src/pages/SignInPage.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from 'axios';
+
+function SignInPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    function login() {
+        const body = {
+            email: email,
+            password: password
+        };
+
+        setTimeout(() => {
+            const promise = axios.post("http://localhost:5000/", body);
+
+            promise.then(res => {
+                navigate("/games");
+            });
+
+            promise.catch(err => {
+                alert(err.response.data.message);
+                setEmail("");
+                setPassword("");
+            });
+        }, 2000);
+    }
+
+    return (
+        <SignInUp>
+            <Logo>
+                <h1>GameShare</h1>
+            </Logo>
+
+            <Form>
+                <input
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Digite o seu email..."
+                    type="email"
+                />
+
+                <input
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword((e.target.value).toString())}
+                    placeholder="Digite a sua senha..."
+                    type="password"
+                />
+
+                <button
+                    type="submit"
+                    onClick={login}>Entrar</button>
+
+            </Form>
+
+            <Link to="/sign-up">
+                <Registration>Não tem uma conta? Cadastre-se!</Registration>
+            </Link>
+        </SignInUp>
+    )
+};
+
+export default SignInPage;
+
+const SignInUp = styled.div`
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Logo = styled.div`
+    width: 40vh;
+    height: 15vh;
+    margin-top: 10vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    h1{
+        font-family: 'Saira Stencil One', cursive;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 7vh;
+    }
+`;
+
+const Form = styled.div`
+    margin-top: 7vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    input{
+        width: 32vh;
+        height: 6vh;
+        padding-left: 4vh;
+        margin-bottom: 3vh;
+        border-radius: 5px;
+    }
+
+    button{
+        width: 37vh;
+        height: 6vh;
+        margin-top: 1vh;
+        border-radius: 5px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        font-family: 'Lexend Deca', sans-serif;
+        font-weight: 400;
+        font-size: 3vh;
+    }
+`;
+
+const Registration = styled.div`
+    width: 50vh;
+    height: 4vh;
+    margin-top: 4vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-family: 'Lexend Deca', sans-serif;
+    font-weight: 400;
+    font-size: 2vh;
+`;
